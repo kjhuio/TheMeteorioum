f9ca00196dc3d5357c6b7f51c922ddeba1734fca
qwe0412_ <82296899+kjhuio@users.noreply.github.com>
2026-09-21 16:23:39 +0900

    feat(cede&workflows): Remove deprecated api & Add Qodana Workflow

--- src/main/java/com/qwe0412/meteorium/Meteorium.java
+++ src/main/java/com/qwe0412/meteorium/Meteorium.java
@@ -54,7 +54,7 @@ public class Meteorium {
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
