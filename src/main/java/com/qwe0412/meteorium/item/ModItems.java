9ad9179f03629a673089a9a9217db57e319fa083
qwe0412_ <82296899+kjhuio@users.noreply.github.com>
2026-09-22 16:52:23 +0900

    YEAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/main/java/com/qwe0412/meteorium/item/ModItems.java
+++ src/main/java/com/qwe0412/meteorium/item/ModItems.java
@@ -1,17 +1,33 @@
 package com.qwe0412.meteorium.item;
 
 import com.qwe0412.meteorium.Meteorium;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SmithingTemplateItem;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.List;
+
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Meteorium.MODID);
 
+    private static final ResourceLocation EMPTY_SLOTS_METEORIUM_UPGRADE = ResourceLocation.fromNamespaceAndPath("meteorium", "empty_slots_meteorium_upgrade");
+
     public static final DeferredItem<Item> METEORIUM_UPGRADE = ITEMS.register(
             "meteorium_upgrade",
-            () -> new Item(new Item.Properties())
+            () -> new SmithingTemplateItem(
+                    Component.translatable("item.minecraft.smithing_template.netherite_upgrade.applies_to").withStyle(ChatFormatting.BLUE),
+                    Component.translatable("item.meteorium.meteorium_ingot").withStyle(ChatFormatting.BLUE),
+                    Component.translatable("upgrade.meteorium.meteorium_upgrade").withStyle(ChatFormatting.GRAY),
+                    Component.translatable("item.minecraft.smithing_template.netherite_upgrade.base_slot_description"),
+                    Component.translatable("item.meteorium.meteorium_ingot.add"),
+                    List.of(),
+                    List.of(EMPTY_SLOTS_METEORIUM_UPGRADE)
+            )
     );
 
     public static void register(IEventBus eventBus) {
