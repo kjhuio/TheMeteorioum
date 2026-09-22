b8df3d6e9102ff415244e89430ba39e604af2708
qwe0412_ <82296899+kjhuio@users.noreply.github.com>
2026-09-22 18:24:57 +0900

    feat(datagen): Add Item Tag Provider

--- src/main/java/com/qwe0412/meteorium/datagen/ModBlockTagProvider.java
+++ src/main/java/com/qwe0412/meteorium/datagen/ModBlockTagProvider.java
@@ -1,4 +1,28 @@
 package com.qwe0412.meteorium.datagen;
 
-public class ModBlockTagProvider {
+import com.qwe0412.meteorium.Meteorium;
+import com.qwe0412.meteorium.block.ModBlocks;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagProvider extends BlockTagsProvider {
+    public ModBlockTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, Meteorium.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.STAR_CINDER.get())
+                .add(ModBlocks.STARDUST_REVERIE.get());
+        tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.STAR_CINDER.get())
+                .add(ModBlocks.STARDUST_REVERIE.get());
+    }
 }
